6d3dd187b8379b23b87f9c907a2c11a7684b1340
sato-kodai <satoverphone@gmail.com>
2020-10-17 14:03:24 +0900

    仮データの一覧表示実装

--- chart/views.py
+++ chart/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView
+from .models import Statement 
 
 class IndexView(TemplateView):
   template_name = 'chart/index.html'
+
+  def get_context_data(self, **kwargs):
+    statement_list = Statement.objects.all().order_by('company')
+    params = {'statement_list': statement_list,}
+    return params
